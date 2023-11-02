e34688b64bee09ea4eb5368cb0590521f1642695
Erick Placides <103293578+ErickPlacidesMartins@users.noreply.github.com>
2023-11-02 18:39:18 -0300

    commit.inicial
    Primeiro commit Site

---
+++ assets/js/javascript.js
@@ -0,0 +1,20 @@
+// Recupera o elemento do contador de acessos
+var accessCountElement = document.querySelector('.access-count');
+
+// Verifica se já existe um valor de contador armazenado no localStorage
+var accessCount = localStorage.getItem('accessCount');
+
+// Se não houver valor no localStorage, começa com 0
+if (accessCount === null) {
+    accessCount = 0;
+}
+
+// Atualiza o contador exibido na página
+accessCountElement.textContent = accessCount;
+
+// Incrementa o contador quando a página é carregada
+accessCount++;
+accessCountElement.textContent = accessCount;
+
+// Armazena o novo valor no localStorage
+localStorage.setItem('accessCount', accessCount);
